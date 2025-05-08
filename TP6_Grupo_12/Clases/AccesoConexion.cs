0150d5ea260988e021134b44c3b4836c86e7d28f
camimoy <camimoyano2006@gmail.com>
2025-05-08 20:07:11 -0300

    cami commit
    conexion y constructor en la clase

--- TP6_Grupo_12/Clases/AccesoConexion.cs
+++ TP6_Grupo_12/Clases/AccesoConexion.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Data;
+
 
 namespace TP6_Grupo_12.Clases
 {
     public class AccesoConexion
     {
-        private string stringConnection;// <-- aca va la cadena de conexion
+        private string stringConnection = "Data Source=localhost\\sqlexpress; Initial Catalog=Neptuno; Integrated Security=True";
+   
+        public AccesoConexion() { }//constructor
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(stringConnection);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception exception)
+            {
+                return null;
+            }
+        }
     }
 }
