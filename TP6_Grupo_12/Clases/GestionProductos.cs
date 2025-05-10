c3ec2800486bd05ca63479dae88e20bdad8ecd9d
ValenDuarte1918 <valentinduarte1@hotmail.com>
2025-05-10 10:58:04 -0300

    Añadi método ObtenerTodosLosProductos
    Implemente el método `ObtenerTodosLosProductos` para recuperar todos los productos de la base de datos. Además, actualice el método `ArmarParametrosEliminarProductos` para trabajar con el objeto `Producto`, cambiando el parámetro SQL de `@IdLibro` a `@IdProducto`.

--- TP6_Grupo_12/Clases/GestionProductos.cs
+++ TP6_Grupo_12/Clases/GestionProductos.cs
@@ -27,13 +27,20 @@ namespace TP6_Grupo_12.Clases
 
             return dataSet.Tables[nombreTabla];
         }
+        // Obtener todos los productos
+        public DataTable ObtenerTodosLosProductos()
+        {
+            string consultaSQL = "SELECT * FROM Productos";
+            return ObtenerTabla("Productos", consultaSQL);
+        }
+
         // Declarar el método ArmarParametrosEliminarProductos
-        //public void ArmarParametrosEliminarProductos(ref SqlCommand sqlCommand, Libro libro)
-        //{
-        //    SqlParameter sqlParameter = new SqlParameter();
-        //    sqlParameter = sqlCommand.Parameters.Add("@IdLibro", SqlDbType.Int);
-        //    sqlParameter.Value = libro.IdLibro;
-        //}
+        public void ArmarParametrosEliminarProductos(ref SqlCommand sqlCommand, Producto producto)
+        {
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter = sqlCommand.Parameters.Add("@IdProducto", SqlDbType.Int);
+            sqlParameter.Value = producto.IdProducto;
+        }
 
         public bool ActualizarProducto(Producto product)
         {
