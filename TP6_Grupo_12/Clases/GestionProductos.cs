f0bcc45702382ead3e18fa8698404977ba78073e
Dante's Noodledit <114444461+Noodledit@users.noreply.github.com>
2025-05-11 01:02:30 -0300

    Revert "Franco"

--- TP6_Grupo_12/Clases/GestionProductos.cs
+++ TP6_Grupo_12/Clases/GestionProductos.cs
@@ -4,24 +4,13 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Linq;
 using System.Web;
-<<<<<<< HEAD
-=======
 using System.Runtime.Remoting.Messaging;
->>>>>>> origin
 
 namespace TP6_Grupo_12.Clases
 {
     public class GestionProductos
     {
         public GestionProductos() 
-<<<<<<< HEAD
-        {
-
-        }
-
-        private DataTable ObtenerTabla(string nombreTabla, string consultaSQL)
-=======
->>>>>>> origin
         {
 
         }
@@ -45,14 +34,6 @@ namespace TP6_Grupo_12.Clases
             return ObtenerTabla("Productos", consultaSQL);
         }
 
-<<<<<<< HEAD
-        }
-
-
-
-    }
-}
-=======
         // Declarar el método ArmarParametrosEliminarProductos
         public void ArmarParametrosEliminarProductos(ref SqlCommand sqlCommand, Producto producto)
         {
@@ -108,4 +89,3 @@ namespace TP6_Grupo_12.Clases
     }
 
 }
->>>>>>> origin
