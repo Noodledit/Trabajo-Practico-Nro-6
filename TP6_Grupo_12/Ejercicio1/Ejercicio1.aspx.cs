4b08930e7c650c9e54c5c9f2d393aa2e1b0e1e68
Miguel Velásquez <86170247+MrM1nd91@users.noreply.github.com>
2025-05-11 12:09:41 -0300

    Corrección errores

---
+++ TP6_Grupo_12/Ejercicio1/Ejercicio1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TP6_Grupo_12.Clases;
+
+namespace TP6_Grupo_12.Ejercicio_1
+{
+    public partial class Ejercicio1 : System.Web.UI.Page
+    {
+        GestionProductos gestionProductos = new GestionProductos();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                AccesoConexion accesoConexion = new AccesoConexion();
+                if (accesoConexion.GenerarProcedimientosAlmacenados())
+                {
+                    lblMensaje.Text = "Procedimientos almacenados generados correctamente.";
+                }
+                else
+                {
+                    lblMensaje.Text = "Error al generar procedimientos almacenados.";
+                }
+
+                rellenarProductos();
+            }
+        }
+        protected void rellenarProductos()
+        {
+            DataTable dt = gestionProductos.ObtenerTodosLosProductos();
+            gvProductos.DataSource = dt;
+            gvProductos.DataBind();
+        }
+        /// ELIMINAR
+        protected void gvProductos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string idProducto = ((Label)gvProductos.Rows[e.RowIndex].FindControl("lbl_it_idProducto")).Text;
+
+            Producto product = new Producto(Convert.ToInt32(idProducto));
+
+            GestionProductos gestionProducts = new GestionProductos();
+            gestionProducts.EliminarProducto(product);
+
+            rellenarProductos();
+
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+    }
+}
